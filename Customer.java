a71e1819fb946409aa78c22f5d4fb850bb8c88d8
Alves <78097460+Ga-Alves@users.noreply.github.com>
2022-12-05 00:58:07 +0000

    Commit 5

--- Customer.java
+++ Customer.java
@@ -23,9 +23,7 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-
-        double thisAmount = each.getCharge();        
+        Rental each = (Rental) rentals.nextElement();       
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -35,8 +33,8 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
      //add footer lines
